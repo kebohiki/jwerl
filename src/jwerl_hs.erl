2753a21bae9ecdea7f42f4494ff86b9e07596e41
JianBo He <heeejianbo@gmail.com>
2020-08-13 09:19:16 +0800

    Use crypto:mac/3 function on above OTP 23

--- src/jwerl_hs.erl
+++ src/jwerl_hs.erl
@@ -3,12 +3,26 @@
 
 -export([sign/3, verify/4]).
 
+%% OTP 21+
+-ifdef(OTP_RELEASE).
+-if(?OTP_RELEASE >= 23).
+
+sign(ShaBits, Key, Data) ->
+  crypto:mac(hmac, algo(ShaBits), Key, Data).
+
+-else. % < 23, >= 21
 sign(ShaBits, Key, Data) ->
   crypto:hmac(algo(ShaBits), Key, Data).
 
+-endif.
+-else. % < OTP 21
+sign(ShaBits, Key, Data) ->
+  crypto:hmac(algo(ShaBits), Key, Data).
+
+-endif.
+
 verify(ShaBits, Key, Data, Signature) ->
   Signature == sign(ShaBits, Key, Data).
-
 
 algo(256) -> sha256;
 algo(384) -> sha384;
